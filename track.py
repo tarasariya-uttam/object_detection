ebfe3fc0be4ec69a4c9623c4b0f2dd44ad1eb092
Uttam Tarasariya <38030742+tarasariya-uttam@users.noreply.github.com>
2022-07-26 10:34:12 -0400

    Update track.py
    add method to test

--- track.py
+++ track.py
@@ -10,11 +10,40 @@ import numpy as np
 from object_detection import ObjectDetection
 from deep_sort.deep_sort import Deep
 
+def detect(model, frame):
+  '''Given results from yolov5, extract classs ids, scores and boxes as numpy array'''
+
+  results = model(frame)
+  result_pandas = results.pandas().xyxy[0]
+  print(result_pandas)
+  result_list = results.xyxy[0].cpu().detach().tolist()
+  class_ids = []
+  scores = []
+  boxes = []
+
+  for i in range(len(result_list)):
+    class_ids.append(result_list[i][5])
+    scores.append(result_list[i][4])
+    boxes.append(result_list[i][0:4])
+
+  class_ids = np.asarray(class_ids, dtype=np.int32)
+  scores = np.asarray(scores, dtype=np.float64)
+  boxes = np.asarray(boxes, dtype=np.float64)
+
+  return class_ids, scores, boxes
+
+# Load Object Detection
+yolov4 = ObjectDetection("dnn_model/yolov4.weights", "dnn_model/yolov4.cfg")
+yolov4.load_class_names("dnn_model/classes.txt")
+yolov4.load_detection_model(image_size=832, # 416 - 1280
+                        nmsThreshold=0.4,
+                        confThreshold=0.3)
+
 def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
-    message = "Hello, " + name + "!\n"
+    message = "Hello, Uttamkumar " + name + "!\n"
     return Response(message)
 
 if __name__ == '__main__':
